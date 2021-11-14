6cfa4e0d8c7fe42a41ea377c48194265560633a6
Ahmed Sofan <ahmed.sofan.fau@gmail.com>
2021-11-14 19:40:55 +0100

    my package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='za07ryma',
+      version='0.1',
+      author='DSSS',
+      author_email='ahmed.sofan.fau@gmail.com',
+      packages=find_packages(),
+      install_requires=['numpy', 'Pillow', 'ipywidgets'])
